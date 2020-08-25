c0b63adceedd5473737c17df7c81704ca48ed95f
Brandon Kamaka <Bkamaka@gmail.com>
2020-08-25 17:34:33 -0400

    added CORS compatibility

--- server.py
+++ server.py
@@ -2,7 +2,10 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler, BaseHTTPRequestHan
 
 import json
 import cgi
+import os
 import threading
+import urllib
+from http import HTTPStatus
 
 def main():
     h_server = threading.Thread(target=h_run)
@@ -47,6 +50,54 @@ class JSONServer(BaseHTTPRequestHandler):
         self._set_headers()
         self.wfile.write(json.dumps(message).encode())
         
+class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+        This sends the response code and MIME headers.
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self.path)
+            if not parts.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(HTTPStatus.MOVED_PERMANENTLY)
+                new_parts = (parts[0], parts[1], parts[2] + '/',
+                                parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.send_header("Location", new_url)
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND, "File not found")
+            return None
+        try:
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.send_header("Access-Control-Allow-Origin", "*")
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
+
 def j_run(server_class=HTTPServer, handler_class=JSONServer):
     server_address = ('', 49081)
     httpd = server_class(server_address, handler_class)
@@ -55,7 +106,7 @@ def j_run(server_class=HTTPServer, handler_class=JSONServer):
     httpd.serve_forever()
 
 
-def h_run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+def h_run(server_class=HTTPServer, handler_class=CORSHTTPRequestHandler):
     server_address = ('', 49080)
     httpd = server_class(server_address, handler_class)
     print('Starting HTTP server on port 49080')
